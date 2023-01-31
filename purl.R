bd50668543d30f0a0a59d9404144f40aaaa029f5
smstraughan <smstraughan@gmail.com>
2023-01-31 11:35:38 -0700

    updating purl code and adding labs 5 and 6 rmds

--- purl.R
+++ purl.R
@@ -4,9 +4,13 @@ library(knitr)
 knitr::purl(input = "Lab2/README.Rmd", output = "Lab2/lab2.R", documentation = 1)
 
 #Lab3
+knitr::purl(input = "Lab3/README.Rmd", output = "Lab3/lab3.R", documentation = 1)
 
 #Lab4
+knitr::purl(input = "Lab4/README.Rmd", output = "Lab4/lab4.R", documentation = 1)
 
 #Lab5
+knitr::purl(input = "Lab5/README.Rmd", output = "Lab5/lab5.R", documentation = 1)
 
 #Lab6
+knitr::purl(input = "Lab6/README.Rmd", output = "Lab6/lab6.R", documentation = 1)
